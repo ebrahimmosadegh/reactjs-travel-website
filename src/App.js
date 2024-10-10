7288676c423b1e5dd9e46895c73250c0e2186226
ebrahim <ebrahimmosadegh@gmail.com>
2024-10-10 19:04:13 +0330

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Pages/Home";
 function App() {
   return (
     <div>
       <Router>
         <Navbar />
+        <Routes>
+          <Route path="/" exact Component={Home}/>
+        </Routes>
       </Router>
     </div>
   );
