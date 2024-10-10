37fa7cef1504427f4b6a85d3c56e0fb1f4bfd411
ebrahim <ebrahimmosadegh@gmail.com>
2024-10-10 09:37:48 +0330

    initial project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
