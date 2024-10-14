5bb1edfe092fd37b1c0a6d7805d491fdb8fd581b
ebrahim <ebrahimmosadegh@gmail.com>
2024-10-14 13:42:15 +0330

    erstellen Anmeldung seit

---
+++ src/Components/Pages/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../App.css'
+function SignUp() {
+    return(
+        <div className="sign-up" style={{backgroundImage:`url(image/)`}}>
+
+        </div>
+    )
+    
+}
+
+export default SignUp;
